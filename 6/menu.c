d66ff8342194e54a83fd36634085c308a4f4ec5f
Aleksandr Borovtsov <alexnavidu@gmail.com>
2017-09-11 11:54:56 +0300

    end 6

---
+++ 6/menu.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// объявляем пользовательсткие прототипы функций
+void menu();
+
+int main()
+{
+    menu();
+    return 0;
+}
+
+void menu()
+{
+    int option;
+    printf( "\n\t What would you like to do? ");
+    printf( "\n\t 1. Square a number ");
+    printf( "\n\t 2. Multiply two numbers");
+    printf( "\n\t 3. Exit \n");
+    scanf("%d", &option);
+    action(option);
+}
